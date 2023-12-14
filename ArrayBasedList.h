c83363331619c6c1cf22e9b5f285c79480548d14
InayaM2003 <153103194+InayaM2003@users.noreply.github.com>
2023-12-14 21:26:02 +0500

    added comments

--- ArrayBasedList.h
+++ ArrayBasedList.h
@@ -11,7 +11,7 @@ class ArrayBasedList {
 private:
     int size;
     int currentPtr;
-
+    //function to increase the size of the array
     void increaseSize() {
         int n = size;
         size *= 2;
@@ -24,12 +24,13 @@ private:
     }
 public:
     T* arr;
-
+    //constructor
     ArrayBasedList<T>() {
         size = 5;
         currentPtr = -1;
         arr = new T[size];
     }
+    //copy constructor
     ArrayBasedList<T>(const ArrayBasedList<T>& other) {
         this->size = other.size;
         this->currentPtr = other.currentPtr;
@@ -38,7 +39,7 @@ public:
             this->arr[i] = other.arr[i];
         }
     }
-
+    //this function inserts the value in a sorted manner
     void insertSorted(T val) {
         bool inBetween = false;
         int index = 0;
@@ -52,7 +53,7 @@ public:
         if (inBetween) insert(val, index);
         else push(val);
     }
-
+    //overloading the assignment operator
     ArrayBasedList<T>& operator=(const ArrayBasedList<T>& other) {
         delete[] this->arr;
         this->size = other.size;
@@ -63,11 +64,11 @@ public:
         }
         return *this;
     }
-
+    //this function checks if the list is empty
     bool empty() {
         return currentPtr == -1;
     }
-
+    //pushes an element to the end of the list
     void push(T data) {
         if (currentPtr == size - 1) {
             increaseSize();
@@ -75,39 +76,39 @@ public:
         currentPtr++;
         arr[currentPtr] = data;
     }
-
+    //clears the list
     void clear() {
         currentPtr = -1;
     }
-
+    //deletes the element at the beginning of the list
     void popFront() {
         deleteItem(0);
     }
-
+    //pushes an element at the beginning of the list
     void pushFront(T val) {
         insert(val, 0);
     }
-
+    //returns the element at the beginning of the list
     T front() {
         if (!empty()) return arr[0];
         return T();
     }
-
+   //removes the element at the end of the list
     void pop() {
         if (empty()) {
             return;
         }
         currentPtr--;
     }
-
+    //returns the last element of the list
     T back() {
         return arr[currentPtr];
     }
-
+    //overloading the subscript operator to access elements
     T& operator[](int i) {
         return arr[i];
     }
-
+    //insert an element at the desired position
     void insert(T data, int pos) {
         if ((pos > (currentPtr + 1)) || (pos < 0)) {
             return;
@@ -122,11 +123,11 @@ public:
         }
         arr[i] = data;
     }
-
+    //returns the size of the list
     int getSize() {
         return currentPtr + 1;
     }
-
+    //deletes the element from the specified position
     void deleteItem(int pos) {
         if ((pos > (currentPtr + 1)) || (pos < 0) || empty()) {
             return;
@@ -136,14 +137,14 @@ public:
         }
         currentPtr--;
     }
-
+    //searches for the given value and returns the index
     int search(T value) {
         for (int i = 0; i <= currentPtr; i++) {
             if (value == arr[i]) return i;
         }
         return -1;
     }
-
+   //overloading the ostream operator to print the list
     friend ostream& operator<<(ostream& out, ArrayBasedList& l) {
         for (int i = 0; i <= l.currentPtr; i++) {
             out << l.arr[i] << " ";
@@ -151,11 +152,11 @@ public:
         out << endl;
         return out;
     }
-
+   //overloading the subscript operator
     T operator[](int index) const {
         return arr[index];
     }
-
+   //destructor
     ~ArrayBasedList() {
         delete[] arr;
     }
